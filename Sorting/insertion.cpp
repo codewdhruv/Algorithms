7b3fa7af85822b8061f6737423120b01ef5dc312
Dhruba Jyoti Chakraborty <61685945+dhrubajyoti89@users.noreply.github.com>
2020-10-04 21:33:24 +0530

    Insertion sort implementation added

---
+++ Sorting/insertion.cpp
@@ -0,0 +1,36 @@
+// Hacktoberfest2020 PR by Dhrubajyoti Chakraborty
+// Insertion sort implementation
+
+#include <iostream>
+using namespace std;
+
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+ 
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+// Driver function
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  cout << "Array in ascending order:\n";
+  printArray(data, size);
+}
