804b72bb0bd4743ed52f20f69eb3543b374c7028
Rabeeh K T <35598587+rabeehkt@users.noreply.github.com>
2020-10-04 22:01:02 +0530

    Update code.py

---
+++ Auction_Number/code.py
@@ -0,0 +1,7 @@
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+state=int(input())
+a,b=input().split()
+n,k=map(int,input().split())
+adiff=(upper.find(b)-upper.find(a))+1
+ndiff=k-n+1
+print((pow(adiff,2))*state*(ndiff//2)*6)
