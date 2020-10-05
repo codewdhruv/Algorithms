7d4eec3ee6b4e26b28ae1da798c1d744e1fcc31a
Rabeeh K T <35598587+rabeehkt@users.noreply.github.com>
2020-10-05 10:56:32 +0530

    Update code.py

--- Auction_Number/code.py
+++ Auction_Number/code.py
@@ -4,4 +4,4 @@ a,b=input().split()
 n,k=map(int,input().split())
 adiff=(upper.find(b)-upper.find(a))+1
 ndiff=k-n+1
-print((pow(adiff,2))*state*(ndiff//2)*6)
+print((pow(adiff,2))*state*((pow(ndiff,4)-(4*n*(n-1)+n)
