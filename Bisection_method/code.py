1f1bd5a2a6bc6f0d1752be05877aca1257cd730e
Rishika <rishikarocky14@gmail.com>
2020-10-09 12:43:50 +0530

    Bisection_method

--- Bisection_method/code.py
+++ Bisection_method/code.py
@@ -1,17 +1,10 @@
-Bisection method : This method is used to find the root of an equation between two numbers a and b, assuming that f(x) is continuous on [a,b] and there exists a value c in [a,b] such that f(c)=0. We first take input two numbers a and b such that f(a)*f(b)<0. Then we take the arithmetic mean of a and b and assign it to c. Then we check whether f(c)=0. If f(c) is not 0, we assign the value of c either to a or b.( a=c if f(a) and f(c) have the same sign or b=c if f(b) and f(c) have the same). We repeat the process till f(c)=0. Since in some cases the correct root is not obtained, f(c) never becomes zero. Hence in order to avoid an infinite process, we input the maximum number of iterations from the user.
-
-# Root finding using quadratic formula
 from math import *
 
-root1=(-3+sqrt(3 * 3 - 4 * 4 * (-3)))/ 8  # quadratic formula is (-b+sqrt(b * b-4*a*c)/2* a).
-                                                # Here a = 4, b = 3, c = -3
+root1=(-3+sqrt(3 * 3 - 4 * 4 * (-3)))/ 8 
 
-print("The root obtained with the quadratic formula is : "+str(root1))  #taking positive root 
+print("The root obtained with the quadratic formula is : "+str(root1))  
 
-# Root finding via bisection method
-
-
-def f(x):                                                   # defining the given quadratic equation
+def f(x):                                                  
     return 4 * x * x + 3 * x - 3
 
 
@@ -19,7 +12,7 @@ a = float(input("Enter a number a such that f(a)>0 :"))
 b = float(input("Enter a number a such that f(b)<0 :"))
 c = 0
 
-if f(a) * f(b) > 0:                                             # we need f(a)>0 and f(b)<0
+if f(a) * f(b) > 0:                                           
     print("Wrong assumptions! The values entered do not support your assumption")
 
 elif f(a) * f(b) == 0:
